66fa23b392a32cd7699742fe340e1be9d3b52f51
Beruli <46021214+Beruli@users.noreply.github.com>
2021-07-05 11:34:23 +0200

    Update superagent_refactor.js

--- superagent_refactor.js
+++ superagent_refactor.js
@@ -1,82 +1,7 @@
-const express = require('express')
-const https = require('https');
-const superagent = require('superagent');
-
-// Lookup all albums from Miley Cyrus
-const ITUNES_URL = 'https://itunes.apple.com/lookup?id=137057909&entity=album&limit=100';
-
-const codeChallengeServer = express()
-const port = 8765
-
-
-let albums = [];
-    
-
-let transformResponse = function (response) {
-    let removeDuplicate = new Set();
-    return Array.from(response.results)
-        // Filter entries with no album name nor thumbnail
-        .filter((item) => {
-            return (!!item.collectionName && !!item.artworkUrl100);
-        })
-        // Filter duplicate album names
-        .filter((item) => {
-            if (removeDuplicate.has(item.collectionName)) {
-                return false;
-            } else {
-                removeDuplicate.add(item.collectionName);
-                return true;
-            }
-        })
-        // Produce result with only necessary entries
-        .map((item) => ({
-            "name": item.artistName,
-            "album": item.collectionName,
-            "thumbnail": item.artworkUrl100
-        }));
-}
-
-
-https.get(ITUNES_URL, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    res.on('end', (d) => {
-        let parsedResults = JSON.parse(data);
-        if (data !== '' && parsedResults?.resultCount) {
-            albums = transformResponse(parsedResults);
-        }
-
-        console.log('Albums transformed: ' + albums.length);
-    });
-
-}).on('error', (err) => {
-    console.error('Cannot get data from iTune: ' + err);
-});
-
-
-codeChallengeServer.get('/codechallenge', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
-    res.send(albums);
-});
-
-
-codeChallengeServer.listen(port, () => {
-    console.log(`Code challenge app listening at http://localhost:${port}`)
-});
-
-
-module.exports = {
-    transformResponse
-}
-
 exports.inviteUser = function (req, res) {
-    var invitationBody = req.body;
-    var shopId = req.params.shopId;
-    var authUrl = "https://url.to.auth.system.com/invitation";
+    let invitationBody = req.body;
+    let shopId = req.params.shopId;
+    let authUrl = "https://url.to.auth.system.com/invitation";
 
     // Check for missing input before triggering superagent.
     if (!shopId) {
@@ -89,6 +14,7 @@ exports.inviteUser = function (req, res) {
         return;
     }
 
+    // Extract inner callback and make it synchronous so we can provide the response back in Express.
     let updateShop = async function (user, invitationId) {
         try {
             const shop = await Shop.findById(shopId).exec();
@@ -108,6 +34,7 @@ exports.inviteUser = function (req, res) {
         }
     }
 
+        // Make supeagent call synchronous so we can provide the response back in Express.
     (async () => {
         try {
             const invitationResponse = await superagent.post(authUrl).send(invitationBody);
